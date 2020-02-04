004d7d4f6d5312e3b08e4103a8b45ffbade5a1b2
Omar Calderon <stivenomar7@gmail.com>
2020-02-04 14:26:11 -0500

    Obtenido los datos desde la Api y mostrados en la interfaz

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ formNombres.addEventListener('submit', getData);
 
 function getData(event) {
     event.preventDefault();
-    //get option selected
+    //Obtencion datos del form
     let pais;
     let numNombres;
     let genero;
@@ -17,8 +17,8 @@ function getData(event) {
 
     numNombres = document.getElementById('numNombres').value;
 
-    console.log(pais, genero , numNombres);
     
+    //Armado de las URL de la API
     let url = '';
 
     url += 'https://uinames.com/api/?'
@@ -36,8 +36,26 @@ function getData(event) {
         url += `amount=${numNombres}&`;
     }
 
-    console.log(url)
+    //Obtencion de los datos y dibujado en el DOM
+    let names;
+    const xhr = new XMLHttpRequest();
 
+    xhr.open('GET',url,true);
+
+    xhr.onload = function(){
+        let names = JSON.parse(xhr.responseText);
+        let listNames = document.getElementById('listNames');
+        listNames.innerHTML = '';
+        let li ;
+        for (const namePerson of names) {
+            li = document.createElement('li');
+            li.classList.add('list-group-item');
+            li.textContent = namePerson.name;
+            listNames.appendChild(li);
+        }
+    }
+
+    xhr.send();
 
 }
 
