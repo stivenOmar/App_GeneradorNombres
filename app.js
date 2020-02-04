8ddece19bb75a1b447d5145168006b4f80da2766
Omar Calderon <stivenomar7@gmail.com>
2020-02-04 09:56:50 -0500

    Obtenida la data del formulario y construidos los endpoints de la API

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 let formNombres;
-
 formNombres = document.getElementById('formNombres');
 
-formNombres.addEventListener('submit',getData);
+formNombres.addEventListener('submit', getData);
 
-function getData(event){
-    console.log('hola');
+function getData(event) {
+    event.preventDefault();
+    //get option selected
+    let pais;
+    let numNombres;
+    let genero;
+
+
+    pais = document.getElementById('menuPaises').value;
+
+    genero = document.getElementById('menuGenero').value;
+
+    numNombres = document.getElementById('numNombres').value;
+
+    console.log(pais, genero , numNombres);
+    
+    let url = '';
+
+    url += 'https://uinames.com/api/?'
+
+    //endpoints
+    if(pais !== '0'){
+        url += `region=${pais}&`;
+    }
+
+    if(genero !== '0'){
+        url += `gender=${genero}&`;
+    }
+
+    if(numNombres !== ''){
+        url += `amount=${numNombres}&`;
+    }
+
+    console.log(url)
+
+
 }
+
